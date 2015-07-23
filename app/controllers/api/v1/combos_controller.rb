e0857f9c68836a0e4caa36d10f05ae9f362256f9
praveen <praveen.beatle@gmail.com>
2015-07-23 05:40:09 +0530

    change

--- app/controllers/api/v1/combos_controller.rb
+++ app/controllers/api/v1/combos_controller.rb
@@ -2,12 +2,10 @@ class Api::v1::CombosController < ApiApplicationController
 	respond_to :json
 
 	def index
-		combos = Combo.all
-		respond_with status: 200, combos.as_json
+		
 	end
 
 	def show
-		combo = Combo.find params[:id]
-		respond_with status: 200, combo.as_json
+		
 	end
 end
--- app/controllers/api/v1/restaurants_controller.rb
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -2,8 +2,10 @@ class Api::v1::RestaurantsController < ApiApplicationController
 	respond_to :json
 
 	def index
+
 	end
 
 	def show
+		
 	end
 end
