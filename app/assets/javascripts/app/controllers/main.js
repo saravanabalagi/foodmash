484c77b97e2824b7f10290fcecca2e3656502285
praveen <praveen.beatle@gmail.com>
2015-08-13 16:30:31 +0530

    progress bar added

--- app/assets/javascripts/app/controllers/main.js
+++ app/assets/javascripts/app/controllers/main.js
@@ -3,9 +3,15 @@
 angular.module('foodmashApp.controllers')
 
 .controller('MainController', ['$scope','$mdSidenav','$location', function($scope, $mdSidenav, $location){
+	$scope.loading = true;
+
 	$scope.toggleList = function() {
 	    $mdSidenav('left').toggle();
 	};
+
+	$scope.$on('$viewContentLoaded', function(){
+		$scope.loading = false;
+	});
 
 	$scope.routeToRoot = function(){
 		$location.path('/');
