9bc28f4cb85333efb6a61e229e1e52b78efc1a79
praveen selvaraj <praveen.beatle@gmail.com>
2015-08-30 19:05:07 +0530

    jsonified delivery_addresses properly

--- app/controllers/api/v1/delivery_addresses_controller.rb
+++ app/controllers/api/v1/delivery_addresses_controller.rb
@@ -6,7 +6,7 @@ class Api::V1::DeliveryAddressesController < ApiApplicationController
 	def index
 		@delivery_addresses = @current_user.delivery_addresses
 		if @delivery_addresses.present?
-			render status: 200, json: @delivery_addresses.as_json
+			render status: 200, json: jsonify_object(@delivery_addresses).as_json
 		else
 			render status: 404, json: {error: "Could not find user's previous addresses!"}
 		end
@@ -15,7 +15,7 @@ class Api::V1::DeliveryAddressesController < ApiApplicationController
 	def create 
 		@delivery_address = parse_and_set(DeliveryAddress.new, params[:data])
 		if @delivery_address.save!
-			render status: 200, json: @delivery_address.as_json
+			render status: 200, json: jsonify_object(@delivery_address).as_json
 		else
 			render status: 422, json: {error: @delivery_address.errors}
 		end
@@ -24,7 +24,7 @@ class Api::V1::DeliveryAddressesController < ApiApplicationController
 	def update
 		@delivery_address = parse_and_set(@delivery_address, params[:data])
 		if @delivery_address and @delivery_address.save!
-			render status: 200, json: @delivery_address.as_json
+			render status: 200, json: jsonify_object(@delivery_address).as_json
 		else
 			render status: 422, json: {error: "Could not update delivery address!"}
 		end
@@ -45,6 +45,18 @@ class Api::V1::DeliveryAddressesController < ApiApplicationController
 
 	def delivery_address_update_params
 		params.require(:data).permit(:name, )
+	end
+
+	def jsonify_object(delivery_addresses)
+		if delivery_addresses.count > 1
+			delivery_addresses_json = delivery_addresses.map do |d|
+				{ id: d.id, name: d.name, address: {line1: d.line1, line2: d.line2, area: d.area, city: d.city, pincode: d.pincode}, geolocation: {latitude: d.latitude, longitude: d.longitude}, phone: d.contact_no, primary: d.primary }
+			end
+			return delivery_addresses_json
+		elsif delivery_addresses.count == 1
+			delivery_address_json = { id: delivery_addresses.id, name: delivery_addresses.name, address: {line1: delivery_addresses.line1, line2: delivery_addresses.line2, area: delivery_addresses.area, city: delivery_addresses.city, pincode: delivery_addresses.pincode}, geolocation: {latitude: delivery_addresses.latitude, longitude: delivery_addresses.longitude}, phone: delivery_addresses.contact_no, primary: delivery_addresses.primary }
+			return delivery_address_json
+		end
 	end
 
 	def parse_and_set(delivery_address, data)
