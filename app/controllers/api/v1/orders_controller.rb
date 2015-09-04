97dabc055f633df69db2db705838737448a172a2
praveen selvaraj <praveen.beatle@gmail.com>
2015-09-04 20:34:19 +0530

    updated orders update return json

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::OrdersController < ApiApplicationController
 	rescue_from ActiveRecord::RecordNotFound, with: :invalid_data
+	prepend_before_filter :authenticate_user_from_token!
 	before_filter :set_order, only: [:update, :destroy, :show]
 	respond_to :json
 
