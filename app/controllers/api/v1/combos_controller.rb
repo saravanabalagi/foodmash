7a83250e13cce816f799d08290cc1758ca2b14bb
praveen selvaraj <praveen.beatle@gmail.com>
2015-09-04 16:29:54 +0530

    streamlined combos json

--- app/controllers/api/v1/combos_controller.rb
+++ app/controllers/api/v1/combos_controller.rb
@@ -5,7 +5,7 @@ class Api::V1::CombosController < ApiApplicationController
 	def index
 		combos = Combo.where(params.permit(:id))
 		if combos
-			render status: 200, json: {success: true, data: combos.as_json(:include => [{:combo_options => {:include => {:combo_option_dishes => {:include => {:dish => {:include => {:restaurant => {only: [:id, :name]}}, only: [:id, :name]} } , only: :id} }, only: [:id, :name, :description]} }, :combo_dishes], only: [:name, :price, :id, :no_of_purchases, :description]) }
+			render status: 200, json: {success: true, data: combos.as_json(:include => [{:combo_options => {:include => {:combo_option_dishes => {:include => {:dish => {:include => {:restaurant => {only: [:id, :name]}}, only: [:id, :name]} } , only: :id} }, only: [:id, :name, :description]} }, {:combo_dishes => {only: [:id, :dish_id]}}], only: [:name, :price, :id, :no_of_purchases, :description]) }
 		else
 			render status: 404, json: {success: false, error: "Could not load the combos!"}
 		end
