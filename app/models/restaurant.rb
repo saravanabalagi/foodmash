3f860ee27819c632134eb12dd9a373819e751792
praveen <praveen.beatle@gmail.com>
2015-09-08 15:49:35 +0530

    changed has_combos to include combo_dishes

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -5,10 +5,16 @@ class Restaurant < ActiveRecord::Base
 	has_many :dish_types, through: :dishes
 
 	def has_combos
-	   Combo.find_by_sql("select * from Combos where Combos.id in
+	   combos_from_cos = Combo.find_by_sql("select * from Combos where Combos.id in
 	  	(select Combo_Options.combo_id from Combo_Options where Combo_Options.id in 
 	  	(select Combo_Option_Dishes.combo_option_id from Combo_Option_Dishes where Combo_Option_Dishes.dish_id in
 	  	(select Dishes.id from Dishes inner join Restaurants where 
 	  	#{self.id} = Dishes.restaurant_id)))")
+
+	   combos = combos_from_cos
+
+	   combos_from_cds = self.dishes.each {|d| combos << d.combos}
+
+	   return combos.uniq
   end
 end
