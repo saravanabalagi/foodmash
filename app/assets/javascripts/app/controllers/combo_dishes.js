0127729a9772bfb7f731786289c48ed5555f8dcd
praveen <praveen.beatle@gmail.com>
2015-09-08 13:08:01 +0530

    combo-dish directified on combo page

--- app/assets/javascripts/app/controllers/combo_dishes.js
+++ app/assets/javascripts/app/controllers/combo_dishes.js
@@ -2,14 +2,12 @@
 
 angular.module('foodmashApp.controllers')
 
-.controller('ComboDishesController', ['$scope', 'ComboDish', '$location', '$q', 'toaster','DishType','Dish', function($scope, ComboDish, $location, $q, toaster, DishType, Dish){
+.controller('ComboDishesController', ['$scope', 'ComboDish', '$q', 'toaster', 'DishType', 'Dish', function($scope, ComboDish, $q, toaster, DishType, Dish){
 
 	$scope.dish_types = {};
 	$scope.combo_dishes = {};
 	$scope.combo_dish = new ComboDish;
-	$scope.updatedComboDish = new ComboDish;	
 	$scope.dishes = {};
-	$scope.dishesForUpdate = {};
 
 	DishType.query().then(function(dish_types){
 		if(dish_types.length > 0){
@@ -20,23 +18,6 @@ angular.module('foodmashApp.controllers')
 	}, function(err){
 		$scope.dish_types = null;
 	});
-
-	$scope.loadDishesForUpdate = function(dish_type_id){
-	var d = $q.defer();
-		Dish.query({dish_type_id: dish_type_id}).then(function(dishes){
-		if(dishes.length > 0){
-			$scope.dishesForUpdate = dishes;
-			d.resolve(dishes);
-		}else{
-			$scope.dishesForUpdate = null;
-			d.resolve(null);
-		}
-	}, function(err){
-		$scope.dishesForUpdate = null;
-		d.reject(err);
-	});
-	return d.promise;
-};
 
 	$scope.loadDishes = function(dish_type_id){
 		var d = $q.defer();
@@ -92,45 +73,6 @@ angular.module('foodmashApp.controllers')
 			$scope.combo_dish = new ComboDish;
 			d.resolve(null);
 		}
-		return d.promise;
-	};
-
-	$scope.setUpdate = function(combo_dish){
-		$scope.updatedComboDish = angular.copy(combo_dish);
-		$scope.loadDishesForUpdate(combo_dish.dish_type_id);
-	};
-
-	$scope.updateComboDish = function(combo_dish, comboDishUpdateCross){
-		var d = $q.defer();
-		if(!comboDishUpdateCross){
-			$scope.updatedComboDish.update().then(function(response){
-				toaster.pop('success', 'Combo Dish was updated!');
-				var index = $scope.combo_dishes.indexOf(combo_dish);
-				if(angular.isNumber(index)){
-					$scope.combo_dishes[index] = $scope.updatedComboDish;
-				}
-				d.resolve(response);
-			}, function(err){
-				toaster.pop('Combo Dish was not updated!');
-				d.reject(err);
-			});
-		}else{
-			$scope.updatedComboDish = new ComboDish;
-			d.resolve(null);
-		}
-		return d.promise;
-	};
-
-	$scope.deleteComboDish = function(combo_dish){
-		var d = $q.defer();
-		combo_dish.delete().then(function(response){
-			toaster.pop('success', 'Combo Dish was deleted!');
-			$scope.combo_dishes.splice($scope.combo_dishes.indexOf(combo_dish), 1);
-			d.resolve(response);
-		}, function(err){
-			toaster.pop('error', 'Combo Dish was not deleted!');
-			d.reject(err);
-		});
 		return d.promise;
 	};
 
---
+++ app/assets/javascripts/app/directives/combo-dish.js
@@ -0,0 +1,81 @@
+'use strict';
+
+angular.module('foodmashApp.directives')
+
+.directive('comboDish', ['ComboDish', '$q', 'toaster', 'DishType', 'Dish', '$location', function(ComboDish, $q, toaster, DishType, Dish, $location){
+
+	return {
+
+		restrict: 'E',
+
+		templateUrl: '/templates/combo-dish.html',
+
+		controller: ['$scope', 'ComboDish', '$q', 'toaster', 'DishType', 'Dish', function($scope, ComboDish, $q, toaster, DishType, Dish){
+
+			$scope.updatedComboDish = new ComboDish;	
+			$scope.dishesForUpdate = {};
+
+			$scope.setUpdate = function(combo_dish){
+				$scope.updatedComboDish = angular.copy(combo_dish);
+				$scope.loadDishesForUpdate(combo_dish.dish_type_id);
+			};
+
+			$scope.loadDishesForUpdate = function(dish_type_id){
+				var d = $q.defer();
+					Dish.query({dish_type_id: dish_type_id}).then(function(dishes){
+					if(dishes.length > 0){
+						$scope.dishesForUpdate = dishes;
+						d.resolve(dishes);
+					}else{
+						$scope.dishesForUpdate = null;
+						d.resolve(null);
+					}
+					}, function(err){
+						$scope.dishesForUpdate = null;
+						d.reject(err);
+					});
+				return d.promise;
+			};
+			
+
+			$scope.updateComboDish = function(combo_dish, comboDishUpdateCross){
+				var d = $q.defer();
+				if(!comboDishUpdateCross){
+					if(!$scope.comboDishUpdateForm.$pristine){
+						$scope.updatedComboDish.update().then(function(response){
+							toaster.pop('success', 'Combo Dish was updated!');
+							var index = $scope.combo_dishes.indexOf(combo_dish);
+							if(angular.isNumber(index)){
+								$scope.combo_dishes[index] = $scope.updatedComboDish;
+							}
+							d.resolve(response);
+						}, function(err){
+							toaster.pop('Combo Dish was not updated!');
+							d.reject(err);
+						});
+					}else{
+						$scope.updatedComboDish = new ComboDish;
+						d.resolve(null);
+					}
+				}
+				return d.promise;
+			};
+
+			$scope.deleteComboDish = function(combo_dish){
+				var d = $q.defer();
+				combo_dish.delete().then(function(response){
+					toaster.pop('success', 'Combo Dish was deleted!');
+					$scope.combo_dishes.splice($scope.combo_dishes.indexOf(combo_dish), 1);
+					d.resolve(response);
+				}, function(err){
+					toaster.pop('error', 'Combo Dish was not deleted!');
+					d.reject(err);
+				});
+				return d.promise;
+			};
+
+		}]
+
+	};
+
+}]);
