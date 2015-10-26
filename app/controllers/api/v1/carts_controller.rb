b4dedb584e1fb07f9c1eb917f514f5fe9f33a75c
Praveen Selvaraj <praveen@Praveens-MacBook-Pro.local>
2015-10-26 16:56:27 +0530

    order history done; added packaging_centre_id to area

--- app/controllers/api/v1/carts_controller.rb
+++ app/controllers/api/v1/carts_controller.rb
@@ -7,7 +7,7 @@ class Api::V1::CartsController < ApiApplicationController
 	respond_to :json
 
 	def history
-		@carts = @current_user.carts if @current_user
+		@carts = @current_user.carts.where("aasm_state != ?", 'not_started') if @current_user
 		if @carts
 			render status: 200, json: {success: true, data: @carts.as_json(:include => {:orders => {:include => [{:order_items => {:include => [{:item => {only: [:id, :name]}}, :category => {only: [:id, :name, :description]}], only: [:id, :quantity]} } ,:product => {only: [:name, :price, :description]}], only: [:id, :quantity, :total]} }, only: [:id, :total, :payment_method, :order_id, :aasm_state, :updated_at]) }
 		else
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151023214858) do
+ActiveRecord::Schema.define(version: 20151026111857) do
 
   create_table "areas", force: :cascade do |t|
     t.string   "name"
     t.integer  "city_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
+    t.integer  "packaging_centre_id"
   end
 
   create_table "carts", force: :cascade do |t|
