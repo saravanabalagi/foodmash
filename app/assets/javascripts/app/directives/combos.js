44c8f2e9e64f308b32aecdf786f3a69fe8d3d28c
Praveen Selvaraj <praveen@Praveens-MacBook-Pro.local>
2016-02-06 14:04:57 +0530

    finished combo pic uploadment :p

--- app/assets/javascripts/app/directives/combos.js
+++ app/assets/javascripts/app/directives/combos.js
@@ -2,7 +2,7 @@
 
 angular.module('foodmashApp.directives')
 
-.directive('combos', ['toaster', 'Combo', '$q', 'PackagingCentre', function(toaster, Combo, $q, PackagingCentre){
+.directive('combos', ['toaster', 'Combo', '$q', 'PackagingCentre', 'Aws', 'Upload', function(toaster, Combo, $q, PackagingCentre, Aws, Upload){
 
 	return {
 
@@ -10,7 +10,7 @@ angular.module('foodmashApp.directives')
 
 		templateUrl: '/templates/combos.html',
 
-		controller: ['$scope', 'toaster', 'Combo', '$q', 'PackagingCentre', function($scope, toaster, Combo, $q, PackagingCentre){
+		controller: ['$scope', 'toaster', 'Combo', '$q', 'PackagingCentre', 'Aws', 'Upload', function($scope, toaster, Combo, $q, PackagingCentre, Aws, Upload){
 
 			$scope.combo = new Combo;
 			$scope.combos = [];
@@ -96,6 +96,34 @@ angular.module('foodmashApp.directives')
 					}
 				});
 				return icon_class;
+			};
+
+			$scope.uploadFiles = function(file, errFiles){
+				if(file){
+					Aws.loadAWS().then(function(aws){
+						file.upload = Upload.upload({
+						    url: 'https://foodmash.s3.amazonaws.com/', //S3 upload url including bucket name
+						    method: 'POST',
+						    data: {
+						        key: 'images/combos/' + Date.now() + '/' + file.name, // the key to store the file on S3, could be file name or customized
+						        AWSAccessKeyId: aws.key,
+						        acl: 'public-read', // sets the access to the uploaded file in the bucket: private, public-read, ...
+						        policy: aws.policy, // base64-encoded json policy (see article below)
+						        signature: aws.signature, // base64-encoded signature based on policy string (see article below)
+						        "Content-Type": file.type != '' ? file.type : 'application/octet-stream', // content type of the file (NotEmpty)
+						        file: file
+						    }
+						});
+
+						file.upload.progress(function(e){ file.progress = Math.min(100, parseInt(100.0 * e.loaded/e.total)); });
+
+						file.upload.then(function(response){
+							$scope.combo.picture = 'https://foodmash.s3.amazonaws.com/' + response.config.data.key;
+							toaster.pop('success', 'Combo pic uploaded!');
+						});
+					});
+					$scope.file = file;
+				}
 			};
 
 			$scope.addCombo = function(){
