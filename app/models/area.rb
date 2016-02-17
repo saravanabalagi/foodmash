b50e7c91e8b4bc50935dff91fee7d28bf72ff3f8
Praveen Selvaraj <praveen@Praveens-MacBook-Pro.local>
2016-02-17 16:14:39 +0530

    changed validations to reflect the change in models

--- app/models/area.rb
+++ app/models/area.rb
@@ -2,6 +2,7 @@ class Area < ActiveRecord::Base
 	before_save {|area| write_attribute(:name, area.name.titleize)}
 	validates :name, presence: true, length: {minimum: 2}
 	validates :city_id, presence: true
+	validates :pincode, presence: true
 	belongs_to :city 
 	belongs_to :packaging_centre
 	has_many :restaurants
--- app/models/delivery_address.rb
+++ app/models/delivery_address.rb
@@ -3,7 +3,7 @@ class DeliveryAddress < ActiveRecord::Base
 	has_many :carts
 	validates :user_id, presence: true
 	validates :contact_no, presence: true, numericality: {only_integer: true}
-	validates_presence_of :pincode, :city, :area, :line1, primary: {default: false}
+	validates_presence_of :area_id, :line1, primary: {default: false}
 	before_save :falsify_true_records
 	before_save :make_primary_for_first_address
 	before_destroy :make_primary_for_first_address
