1ce2f29a4f5c200a6fb62a3a8e3e0253f1e69735
Praveen Selvaraj <praveen@Praveens-MacBook-Pro.local>
2016-02-29 23:20:04 +0530

    changed url to include http

--- app/assets/javascripts/app/controllers/cart.js
+++ app/assets/javascripts/app/controllers/cart.js
@@ -11,8 +11,8 @@ angular.module('foodmashApp.controllers')
 			"productinfo": "a bunch of combos from Foodmash",
 			"firstname": $rootScope.currentUser.name.split(" ")[0],
 			"phone": $rootScope.currentUser.mobile_no,
-			"surl": 'www.foodmash.herokuapp.com/web/payments/success',
-			"furl": 'www.foodmash.herokuapp.com/web/payments/success'
+			"surl": 'http://www.foodmash.herokuapp.com/web/payments/success',
+			"furl": 'http://www.foodmash.herokuapp.com/web/payments/success'
 		};
 	}
 	$scope.cart.delivery_charge = 0;
