21698c2a7cb04bf5224f30e8ab0d8323447f0a5d
Praveen Selvaraj <praveen@Praveens-MacBook-Pro.local>
2016-03-23 18:00:39 +0530

    removed message div; added empty maintenance message

--- app/controllers/api/v1/check_connection_controller.rb
+++ app/controllers/api/v1/check_connection_controller.rb
@@ -15,13 +15,13 @@ class Api::V1::CheckConnectionController < ApiApplicationController
 			title: 'Foodmash Launch',
 			message: 'We are working on something big for you, come back today evening at 7 p.m. !',
 			image: 'http://www.thesaleslion.com/wp-content/uploads/2012/10/blog-launch.jpg',
-			blocking: true
+			blocking: false
 		}
 		settings = {
 
 		}
 		if params[:android_id].present?
-			render status: 200, json: {success: true, data: {versions: versions, maintenance: maintenance, settings: settings}}
+			render status: 200, json: {success: true, data: {versions: versions, maintenance: {}, settings: settings}}
 		else
 			render status: 404, json: {success: false}
 		end 
