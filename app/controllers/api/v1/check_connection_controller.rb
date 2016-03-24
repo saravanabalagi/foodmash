668efddae596e2395896374de99e7512cdfe66c7
Praveen Selvaraj <praveen@Praveens-MacBook-Pro.local>
2016-03-24 12:05:40 +0530

    updated message

--- app/controllers/api/v1/check_connection_controller.rb
+++ app/controllers/api/v1/check_connection_controller.rb
@@ -13,15 +13,15 @@ class Api::V1::CheckConnectionController < ApiApplicationController
 		versions = Version.last
 		maintenance = {
 			title: 'Foodmash Launch',
-			message: 'We are working on something big for you, come back today evening at 7 p.m. !',
+			message: 'We are operational from 7 - 10 p.m.',
 			image: 'http://www.thesaleslion.com/wp-content/uploads/2012/10/blog-launch.jpg',
-			blocking: false
+			blocking: true
 		}
 		settings = {
 
 		}
 		if params[:android_id].present?
-			render status: 200, json: {success: true, data: {versions: versions, settings: settings}}
+			render status: 200, json: {success: true, data: {versions: versions, maintenance: maintenance, settings: settings}}
 		else
 			render status: 404, json: {success: false}
 		end 
