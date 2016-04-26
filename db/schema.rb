8fb4f842f2d80575c0de0d06be038dd696da0102
praveen <praveen.beatle@gmail.com>
2016-04-27 02:11:54 +0530

    fixed feature

--- db/schema.rb
+++ db/schema.rb
@@ -106,6 +106,14 @@ ActiveRecord::Schema.define(version: 20160426042026) do
     t.boolean  "customizable",        default: false
   end
 
+  create_table "contact_us", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "issue"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
   create_table "cuisines", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
