eafd3faf94c5e0970e099b252f1331f1e771f1bd
praveen selvaraj <praveenselvaraj@Marans-MacBook-Air.local>
2016-05-14 15:50:43 +0530

    commented out for bugging

--- app/jobs/send_purchase_confirmation_job.rb
+++ app/jobs/send_purchase_confirmation_job.rb
@@ -3,6 +3,6 @@ class SendPurchaseConfirmationJob < ActiveJob::Base
 
   def perform(user, cart)
   	UserMailer.purchase_confirmation(user, cart).deliver_later!
-  	user.send_sms('Hey, '+ user.name.first +' thank you for choosing Foodmash :) ' + 'Your order worth Rs ' + cart.grand_total.to_s + ' has been confirmed.' + 'We would love to serve you again!')
+  	#user.send_sms('Hey, '+ user.name.split.first + ' thank you for choosing Foodmash :) ' + 'Your order worth Rs ' + cart.grand_total.to_s + ' has been confirmed.' + 'We would love to serve you again!')
   end
 end
