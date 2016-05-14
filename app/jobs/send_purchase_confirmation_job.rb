146252b508ba873285b05f497173a8d4d1cf23a3
praveen selvaraj <praveenselvaraj@Marans-MacBook-Air.local>
2016-05-14 17:18:02 +0530

    formatted sms

--- app/jobs/send_purchase_confirmation_job.rb
+++ app/jobs/send_purchase_confirmation_job.rb
@@ -3,6 +3,6 @@ class SendPurchaseConfirmationJob < ActiveJob::Base
 
   def perform(user, cart)
   	UserMailer.purchase_confirmation(user, cart).deliver_later!
-  	#user.send_sms('Hey, '+ user.name.split.first + ' thank you for choosing Foodmash :) ' + 'Your order worth Rs ' + cart.grand_total.to_s + ' has been confirmed.' + 'We would love to serve you again!')
+  	user.send_sms("Hey "+ user.name.split.first + ", \nThank you for choosing Foodmash :) " + "\nYour order worth Rs " + cart.grand_total.to_s + " has been confirmed.\n" + "We would love to serve you again!")
   end
 end
