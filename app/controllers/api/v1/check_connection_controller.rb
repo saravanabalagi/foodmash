c07ca3dfc998970435b329163b7758d71488ef5c
praveenselvaraj <praveen.beatle@gmail.com>
2016-05-31 09:33:21 +0300

    changed set cart logic; removed maintenence message

--- app/controllers/api/v1/check_connection_controller.rb
+++ app/controllers/api/v1/check_connection_controller.rb
@@ -11,19 +11,19 @@ class Api::V1::CheckConnectionController < ApiApplicationController
 
 	def instantiate
 		versions = Version.last
-		maintenance = {
-			title: 'Welcome to Foodmash!',
-			message: 'We deliver from 7-10 p.m.',
-			image: 'https://s3-ap-southeast-1.amazonaws.com/foodmash-india/assets/welcome.jpg',
-			blocking: false
-		}
+		# maintenance = {
+		# 	title: 'Welcome to Foodmash!',
+		# 	message: 'We deliver from 7-10 p.m.',
+		# 	image: 'https://s3-ap-southeast-1.amazonaws.com/foodmash-india/assets/welcome.jpg',
+		# 	blocking: false
+		# }
 		settings = {
 			 mash_cash: false,
 			 online_payments: false,
 			 verify_user: true
 		}
 		if params[:android_id].present?
-			render status: 200, json: {success: true, data: {versions: versions, maintenance: maintenance, settings: settings}}
+			render status: 200, json: {success: true, data: {versions: versions, settings: settings}}
 		else
 			render status: 404, json: {success: false}
 		end 
