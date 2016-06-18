db714e49d49210ae65e8addcfafdb4bc9ab24600
praveenselvaraj <praveen.beatle@gmail.com>
2016-06-18 14:23:57 +0530

    checks for passwords match

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable
+         :recoverable, :rememberable, :trackable, :validatable
 
   delegate :can?, :cannot?, to: :ability
 
