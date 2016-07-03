114c7839446c6a48a807c9a6bc4177c77105de68
praveenselvaraj <praveen.beatle@gmail.com>
2016-07-03 17:01:21 +0530

    nil if cart not found in validatePromo for android api

--- app/controllers/api/v1/payments_controller.rb
+++ app/controllers/api/v1/payments_controller.rb
@@ -70,7 +70,8 @@ class Api::V1::PaymentsController < ApiApplicationController
  	end
 
  	def apply_promo_code
- 		success, promo_discount, grand_total = @cart.apply_promo_code(params[:data][:promo_code].downcase) || nil, nil, nil
+ 		return invalid_data unless @cart
+ 		success, promo_discount, grand_total = @cart.apply_promo_code(params[:data][:promo_code].downcase)
  	 	if success and promo_discount
  	 		render status: 200, json: {success: success, data: {promo_discount: promo_discount, grand_total: grand_total}}
  	 	else
